f8d818de8e9ae2394019428403275cd0b72c7532
Pau Cangas <paucangas@iesebre.com>
2023-09-19 13:15:07 +0200

    he afegit el primer projecte Java del curs

---
+++ PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Vaig a mostrar el missatge "Hello World"
+        //Shortcut del System.out.println() --> sout
+        //Println fa un salt de linea mentres que el print normal continua el text on estava
+        System.out.println("Hello world!");
+        System.out.println("Segona linea de text");
+        System.out.print("Em diuen Pau");
+        System.out.println(" i això es una prova");
+    }
+}
+
+//Exemple de comentari de una sola linea
+/*
+Exemple de comentari de diverses linies amb un final
+ */
